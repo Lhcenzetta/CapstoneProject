1b814d96e49d199d3ed6d8bc5d72df0c3c307488
Lait-zet <lhcenztta18@gmail.com>
2025-04-15 23:58:21 +0100

    small update

--- lemon-resturant/src/compenents/Nav.js
+++ lemon-resturant/src/compenents/Nav.js
@@ -8,10 +8,10 @@ function Nav() {
       <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
       <ul className="navbar">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="about">About</Link></li>
+        <li><Link to="/about">About</Link></li>
         <li><Link to="/Main">Reservation</Link></li>
         <li><Link to="Login">Login</Link></li>
-        <li><Link to="DevlopedBy">DevelopedBy</Link></li>
+        <li><Link to="/DevlopedBy">DevelopedBy</Link></li>
       </ul>
     </nav>
   );
